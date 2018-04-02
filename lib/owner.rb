654f774f42cb6d8f135dcd2d0fb5f67437f005d8
Kyle Harris <k_harris95@live.com>
2018-04-02 01:46:10 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -70,12 +70,10 @@ class Owner
    end
 
    def sell_pets
-     @pets[:dogs].each { |dog| dog.mood = "nervous"}
-     @pets[:cats].each { |cat| cat.mood = "nervous"}
-     @pets[:fishes].each { |fish| fish.mood = "nervous"}
-     @pets[:dogs] = []
-     @pets[:cats] = []
-     @pets[:fishes] = []
+     @pets.each { |species, animals| 
+       animals.each { |animal| animal.mood == "nervous" }
+      animals.clear
+    }
    end
 
    def list_pets
